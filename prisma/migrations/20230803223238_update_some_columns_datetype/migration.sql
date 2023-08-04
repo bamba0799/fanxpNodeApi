e6c6b0e4623f1a52b6127d542507d3ab3b86cd49
David <david.sehi2016@gmail.com>
2023-08-04 10:11:50 +0000

    new db push
    use `unique` index instead of `pk`

--- prisma/migrations/20230803223238_update_some_columns_datetype/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `Match` MODIFY `date` DATE NOT NULL;
-
--- AlterTable
-ALTER TABLE `MatchTicketUser` MODIFY `date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE `Player` MODIFY `birthday` DATE NOT NULL;
