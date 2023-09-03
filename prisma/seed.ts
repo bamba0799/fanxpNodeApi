cb0b3d8ce62d6b7a4dfcdee91c289130566b91d2
David <david.sehi2016@gmail.com>
2023-09-03 18:30:30 +0000

    add user role field

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,6 +7,7 @@ async function main() {
     data: {
       username: 'admin',
       password: '$2y$10$bGCs6SMtui1Vu7.Ii5Qm8.p069FOuaoBQLpQszqBDYjhdF/u8EMke', // sufanxp (salt = 10)
+      role: 'SU_ADMIN',
     },
   });
 }
